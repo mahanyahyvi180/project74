c4c22e514740232517c458d676dddbd196438c83
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-10 10:28:29 +0330

    Add files via upload

---
+++ ConsoleApp74/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp74
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetDay(78));
+            Console.ReadLine();
+        }
+
+        static string GetDay(int dayNum) {
+
+            string dayName;
+            switch (dayNum)
+            {
+                case 0:
+                    dayName = "sunday";
+                    break;
+
+                case 1:
+                    dayName = "monday";
+                    break;
+
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+
+                case 3:
+                    dayName = "wednesday";
+                    break;
+
+                case 4:
+                    dayName = "Thursday";
+                    break;
+
+                case 5:
+                    dayName = "friday";
+                    break;
+
+                default:
+                    dayName="Invalid Day Number";
+                    break;
+            }
+            return dayName;
+        }
+    }
+}
